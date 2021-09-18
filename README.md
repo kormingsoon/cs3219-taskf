cbfceeeba52e9a3db0b6a5d029fd03a0ef4fc4fa
kormingsoon <kormingsoon@gmail.com>
2021-09-19 00:18:49 +0800

    Update index.js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cs3219-taskf
+
+## **Redis Caching Task**
+
+Do note that the redis caching task f is indepedent of task b.
+
--- index.js
+++ index.js
@@ -4,20 +4,22 @@ const cors = require("cors")
 const Redis = require('redis')
 
 const client = Redis.createClient()
-const DEFAULT_EXPIRATION = 3600
+const DEFAULT_EXPIRATION = 10
 const app = express()
 
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
 
-function cache(req, res, next) {
-    client.get('photos', (err, data) => {
-        if (err) throw err;
+keys = 'photos'
 
-        if (data) {
-            console.log("Redis cache!");
-            res.send(data)
+function cache(req, res, next) {
+    client.get(keys, (err, photos) => {
+        if (err) throw err;
+        if (photos) {
+            console.log("cache hit!");
+            res.send(photos)
         } else {
+            console.log("cache miss!");
             next();
         }
     });
@@ -25,13 +27,11 @@ function cache(req, res, next) {
 
 app.get("/", cache, async(req, res) => {
     axios.get("https://jsonplaceholder.typicode.com/photos")
-        .then(function (respond) {
-            const data = JSON.stringify(respond.data);
-
-            client.setex('photos', 10, data);
-
+        .then(function (stuffToSend) {
+            const data = JSON.stringify(stuffToSend.data);
+            client.setex(keys, DEFAULT_EXPIRATION, data);
             res.send({
-                data: respond.data
+                data: stuffToSend.data
             });
         });
 })
